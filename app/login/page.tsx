ce16c8917339d0c392ddac6eee5346bcf5d4261b
magmom95 <magmom95@naver.com>
2024-09-14 19:13:09 +0900

    finisih html

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,24 @@
-import Link from "next/link";
-
-export default function Home() {
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+import SocialLogin from "@/components/social-login";
+export default function LogIn() {
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen p-6">
-      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥕</span>
-        <h1 className="text-4xl ">당근</h1>
-        <h2 className="text-2xl">바오 마켓에 어서오세요!</h2>
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-
-      <div className="flex flex-col items-center gap-3 w-full">
-        <Link href="/create-account" className="primary-btn text-lg py-2.5">
-          시작하기
-        </Link>
-        <div className="flex gap-2">
-          <span>이미 계정이 있나요?</span>
-          <Link href="/login" className="hover:underline">
-            로그인
-          </Link>
-        </div>
-      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput type="email" placeholder="Email" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="Log in" />
+      </form>
+      <SocialLogin />
     </div>
   );
 }

54810a3d2ad9675dbc0a1a1fa08a7807e2b44b54
magmom95 <magmom95@naver.com>
2024-09-22 17:20:37 +0900

    verification

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -53,8 +53,6 @@ export async function createAccount(prevState: any, formData: FormData) {
   // }
   const result = formSchema.safeParse(data);
   if (!result.success) {
-    console.log(result.error.flatten());
-
     return result.error.flatten();
   } else {
     console.log(result.data);
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -3,9 +3,16 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./action";
+import { smsLogIn } from "./action";
+import { error } from "console";
+
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -13,14 +20,25 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );

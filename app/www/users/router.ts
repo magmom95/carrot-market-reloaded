596a9cee583ceb8245e8780192e3c1a60906957d
magmom95 <magmom95@naver.com>
2024-10-23 00:34:25 +0900

    ing

--- app/www/users/router.ts
+++ app/www/users/router.ts
@@ -1,7 +1,6 @@
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  console.log(request);
   return Response.json({
     ok: true,
   });
@@ -9,6 +8,5 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
-  console.log("log the user in!!!");
   return Response.json(data);
 }

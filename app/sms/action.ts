85fbdec074eba2b2a7a5cdb052d10c3f242f0191
magmom95 <magmom95@naver.com>
2024-10-26 16:26:02 +0900

    ing

--- app/sms/action.ts
+++ app/sms/action.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import crypto from "crypto";
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
 const phoneSchema = z
   .string()
@@ -16,6 +18,23 @@ const tokenSchema = z.coerce.number().min(100000).max(999999);
 
 interface ActionState {
   token: boolean;
+}
+
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    return getToken();
+  } else {
+    return token;
+  }
 }
 
 export async function smsLogIn(prevState: ActionState, formData: FormData) {

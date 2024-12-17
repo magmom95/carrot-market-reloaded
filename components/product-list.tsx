8534fa0a8d1335a271eb996f9344516a8d30f3b1
magmom95 <magmom95@naver.com>
2024-12-18 01:12:22 +0900

    infinite

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import ListProduct from "./list-product";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 interface ProductListProps {
   initialProducts: InitialProducts;
@@ -9,6 +9,7 @@ interface ProductListProps {
 export default function ProductList({ initialProducts }: ProductListProps) {
   const [products, setProducts] = useState(initialProducts);
   const [isLoading, setIsLoading] = useState(false);
+  const trigger = useRef(1);
   const onLoadMoreClick = async () => {
     setIsLoading(true);
     const newProducts = await getMoreProducts(1);
@@ -20,13 +21,9 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      <button
-        onClick={onLoadMoreClick}
-        disabled={isLoading}
-        className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
-      >
+      <span className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95">
         {isLoading ? "로딩 중" : "Load more"}
-      </button>
+      </span>
     </div>
   );
 }
